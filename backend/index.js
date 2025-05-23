c33a961ae8e8403cdd7c090fdf2b7675b97fd6f5
nischay92 <nischayn92@gmail.com>
2025-05-23 13:35:43 -0400

    Deployment requirements

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
-// index.js
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import weatherRoutes from './routes/weatherRoutes.js';
 
 dotenv.config();
@@ -11,15 +13,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Weather API is running...'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use('/api/weather', weatherRoutes);
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
+    app.listen(process.env.PORT || 5000, () =>
+      console.log(`Server running on port ${process.env.PORT || 5000}`)
     );
   })
   .catch((err) => console.error('MongoDB connection error:', err));
